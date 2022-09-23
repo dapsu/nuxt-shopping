d1a44192f74b7f11cbde153db1a4af55adb8e926
dapsu <greenrock46@gmail.com>
2022-09-23 18:18:42 +0900

    feat: backend 코드 클론

--- pages/main.vue
+++ pages/main.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <p>메인 페이지입니다.</p>
+    <div>
+      {{ products }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  data() {
+    return {
+      products: [],
+    }
+  },
+  async created() {
+    const response = await axios.get('http://localhost:3000/products');
+    this.products = response.data;
+  }
 }
 </script>
 
