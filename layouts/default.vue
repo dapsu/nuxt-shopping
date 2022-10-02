ae3bba5f1632f86b070f8644907b7773bcc60d71
dapsu <greenrock46@gmail.com>
2022-10-02 18:38:12 +0900

    feat: 상품 디테일 라우트 생성(동적 라우팅)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <header>
-      <h1>{{ $route.name }} 페이지</h1>
-      <Nuxt-link to="/">홈페이지</Nuxt-link>
-      <Nuxt-link to="/main">메인 페이지</Nuxt-link>
-      <Nuxt-link to="/product">상품 페이지</Nuxt-link>
+      <h1>
+        <Nuxt-link to="/">
+          Nuxt Shopping
+        </Nuxt-link>
+      </h1>
     </header>
     <Nuxt />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    <p>nuxt 시작하기</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text">
+      </div>
+      <ProductList></ProductList>
+    </main>
   </div>
 </template>
 
 <script>
+// import axios from 'axios';
+import ProductList from '~/components/ProductList.vue';
 export default {
-
+  components: { ProductList },
+  // async aysncData() {
+  //   const response = await axios.get('http://localhost:3000/products');
+  //   const products = response.data;
+  //   return { products };
+  // },
 }
 </script>
 
-<style>
-
+<style scoped>
+  .flex {
+    display: flex;
+    justify-content: center;
+  }
+  .item {
+    display: inline-block;
+    width: 400px;
+    height: 300px;
+    text-align: center;
+    margin: 0 0.5rem;
+    cursor: pointer;
+  }
+  .product-image {
+    width: 400px;
+    height: 250px;
+  }
+  .app {
+    position: relative;
+  }
+  .cart-wrapper {
+    position: sticky;
+    float: right;
+    bottom: 50px;
+    right: 50px;
+  }
+  .cart-wrapper .btn {
+    display: inline-block;
+    height: 40px;
+    font-size: 1rem;
+    font-weight: 500;
+  }
 </style>
